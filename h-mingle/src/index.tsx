1a2c409aac897dbb692b973682ba2f94bb1e14fb
flamozzi <pjs99047@gmail.com>
2023-12-07 16:56:21 +0900

    HashRouter

--- h-mingle/src/index.tsx
+++ h-mingle/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 // import ReactDOM from 'react-dom/client'; // React 18
+import { HashRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom'; // React 17
 import './index.css';
 import App from './App';
@@ -21,9 +22,9 @@ import { BrowserRouter } from 'react-router-dom';
 // React 17
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
