324880b56a5172360e7d807a530dfe4f1c90ae9f
flamozzi <pjs99047@gmail.com>
2023-12-15 14:08:56 +0900

    Change base url

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Axios 인스턴스 생성
 const instance = axios.create({
-  baseURL: 'https://h-mingle.netlify.app/',
+  baseURL: 'http://3.34.190.110:8080/', // 기본 요청 경로
 });
 
 export default instance;
